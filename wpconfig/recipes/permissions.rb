be251ddc7ecb1fedd90744c9c78e2415d449c3ca
Ashwin Murali <ashwin.murali@gmail.com>
2016-03-25 14:51:48 +0530

    adding permissions recipe

--- wpconfig/recipes/permissions.rb
+++ wpconfig/recipes/permissions.rb
@@ -1,3 +1,4 @@
 execute "change directory permissions for wp-content" do
   command "chmod -R 774 /srv/www/wordpress/current/wp-content/"
+  command "chown -R deploy:www-data /srv/www/wordpress/current/wp-content"
 end
